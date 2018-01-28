018b4f662e45eea09e8154729dd6a88764f8327a
Yuhsiang Tsai <yhmtsai@gmail.com>
2018-01-28 21:45:39 +0800

    write a simple_spmv of example

--- include/ginkgo.hpp
+++ include/ginkgo.hpp
@@ -36,6 +36,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #include "core/matrix/csr.hpp"
+#include "core/matrix/ell.hpp"
+#include "core/matrix/hyb.hpp"
 #include "core/matrix/dense.hpp"
 #include "core/solver/cg.hpp"
 
